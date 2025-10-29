12babd8bad9a33e37aed3ef1c915fa00bc307ad9
Jorge Alvarez Pinto <jorgealvarezpinto@MacBook-Pro-de-Jorge.local>
2025-10-29 11:26:43 -0300

    fix main4

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const envOrigins = (process.env.FRONTEND_URL ?? '')
-    .split(',')
-    .map((value) => value.trim())
-    .filter((value) => value.length > 0);
-  const allowedOrigins =
-    envOrigins.length > 0 ? envOrigins : ['http://localhost:8080'];
+  // const envOrigins = (process.env.FRONTEND_URL ?? '')
+  //   .split(',')
+  //   .map((value) => value.trim())
+  //   .filter((value) => value.length > 0);
+  // const allowedOrigins =
+  //   envOrigins.length > 0 ? envOrigins : ['http://localhost:8080'];
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -22,23 +22,23 @@ async function bootstrap() {
   );
   const isProd = process.env.NODE_ENV === 'production';
   app.use('/static', express.static(join(__dirname, '..', 'public')));
-  if(!isProd){
-    app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(
-        new Error(`Origin ${origin} is not allowed by CORS policy.`),
-        false,
-      );
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-    credentials: true,
-    maxAge: 86400,
-  });
-}
+//   if(!isProd){
+//     app.enableCors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         return callback(null, true);
+//       }
+//       return callback(
+//         new Error(`Origin ${origin} is not allowed by CORS policy.`),
+//         false,
+//       );
+//     },
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+//     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+//     credentials: true,
+//     maxAge: 86400,
+//   });
+// }
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
